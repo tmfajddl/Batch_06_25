17ec6737b14a55088941bb706b0a5b1ed55cfc01
tmfajddl <tmfajddl12@gmail.com>
2025-06-24 13:37:48 +0900

    고객 상품 등록

---
+++ src/main/java/com/koreait/exam/batch_25_06/app/base/DevInitData.java
@@ -0,0 +1,4 @@
+package com.koreait.exam.batch_25_06.app.base;
+
+public class DevInitData {
+}
---
+++ src/main/java/com/koreait/exam/batch_25_06/job/withParam/WithParamJobConfig.java
@@ -0,0 +1,4 @@
+package com.koreait.exam.batch_25_06.job.withParam;
+
+public class WithParamJobConfig {
+}
